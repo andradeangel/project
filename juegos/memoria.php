a6cb56d2db4d4104c7baba8c571c23a57f65c06a
Ángel <elfoldine@gmail.com>
2024-10-10 15:17:50 -0400

    Atualizacion de los juegos, pendiente monitoreooo

--- juegos/memoria.php
+++ juegos/memoria.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Juego de Memoria</title>
+    <title>Memorizando</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <style>
